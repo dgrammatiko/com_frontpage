9036d94847e5a0137bacf8f1ac08bfc97fe5059e
dgrammatiko <d.grammatiko@gmail.com>
2021-01-20 11:47:38 +0100

    Standardise the module positions

--- src/v4_component/site/tmpl/home/default.php
+++ src/v4_component/site/tmpl/home/default.php
@@ -14,11 +14,12 @@ use Joomla\CMS\Factory;
 $document = Factory::getDocument();
 $renderer = $document->loadRenderer('modules');
 
-// Alternative method
+// Set the Document Title & Description (SEO)
 $document->setTitle('Creating perfect grid layouts');
 $document->setDescription('The best grid creation tool');
 
-// The actual page:
+// The actual page renders 3 Module Positions
 echo
-$renderer->render('some-module-position', array('style' => 'raw'), null),
-$renderer->render('some-other-module-position', array('style' => 'raw'), null);
+$renderer->render('front-page-top', array('style' => 'raw'), null),
+$renderer->render('front-page-middle', array('style' => 'raw'), null),
+$renderer->render('front-page-bottom', array('style' => 'raw'), null);
