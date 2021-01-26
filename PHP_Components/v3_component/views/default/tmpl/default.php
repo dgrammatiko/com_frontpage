193bdba8c4d12d31176fa446774d5886013204d5
dgrammatiko <d.grammatiko@gmail.com>
2021-01-26 13:43:06 +0100

    some typos

--- PHP_Components/v3_component/views/default/tmpl/default.php
+++ PHP_Components/v3_component/views/default/tmpl/default.php
@@ -2,7 +2,7 @@
 /**
  * {{componentName}} Joomla Component
  *
- * @copyright  Copyright (C) 2016 Michael Babker. All rights reserved.
+ * @copyright  Copyright (C) 2016 Dimitris Grammatikogiannis. All rights reserved.
  * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
 defined('_JEXEC') or die;
