bd14c4457372d010d3aa743e47c83d4775817ede
dgrammatiko <d.grammatiko@gmail.com>
2021-04-07 16:35:21 +0200

    x

--- 11ty/_data/getCss.js
+++ 11ty/_data/getCss.js
@@ -1,12 +1,12 @@
 const { readFile } = require('fs').promises;
 
 module.exports = async function() {
-  let cssContents = await readFile('node_modules/naf-css/css/vars.min.css', { encoding: 'utf8' });
-  cssContents += await readFile('node_modules/naf-css/css/base.min.css', { encoding: 'utf8' });
-  cssContents += await readFile('node_modules/naf-css/css/forms.min.css', { encoding: 'utf8' });
-  cssContents += await readFile('src/assets/css/css.min.css', { encoding: 'utf8' });
-
   return {
-    css: cssContents
+    css: (await Promise.all([
+    await readFile('node_modules/naf-css/css/vars.min.css', { encoding: 'utf8' }),
+    await readFile('node_modules/naf-css/css/base.min.css', { encoding: 'utf8' }),
+    await readFile('node_modules/naf-css/css/forms.min.css', { encoding: 'utf8' }),
+    await readFile('src/assets/css/css.min.css', { encoding: 'utf8' })
+  ])).join('')
   };
 }
